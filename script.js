dc366aa6e8050e26d1b0cbb67086fe983142dbfa
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2024-09-08 19:15:04 -0300

    Remove Functionality

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-let i = 0;
-function addItem(){
-    var title = document.getElementById("itemTitle").value;
-    var textnode = document.createTextNode(title);
-    var item = document.createElement("li");
-    var removeBtn = document.createElement("button");
-    removeBtn.innerHTML = "X";
-    item.id = "item" + i++;
-    item.appendChild(removeBtn);
-    item.appendChild(textnode);
-    document.getElementById("list").appendChild(item);
+const listContainer = document.getElementById("listContainer");
+const taskTitle = document.getElementById("taskTitle");
+
+function addTask(){
+    if (taskTitle.value = ""){
+        alert("Input value null");
+    }
+    else{
+        let li = document.createElement("li");
+        let.innerHTML = taskTitle.value;
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        li.appendChild(span);
+        listContainer.appendChild(li);
+    }
+    taskTitle.value = "";
+    loadTask();
 }
 
-function removeItem(){
-    var item = document.getElementById("Item"+ i);
-    item.remove();
-    i--;
+listContainer.addEventListener("click", taskFunctions(element));
+
+function taskFunctions(element){
+    if (element.target.tagName = "li"){
+        element.target.classList.toggle("Checked");
+        loadTask();
+    }
+    else if (element.taget.tagName = "span"){
+        element.taget.remove();
+        loadTask();
+    }
 }
+
+function saveTask(){
+    localStorage.setItem("taskList",listContainer.innerHTML);
+}
+
+function loadTask(){
+    listContainer.innerHTML = localStorage.getItem("taskList");
+}
+
+loadTask();
